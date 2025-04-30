da51066bbdbd56c242bb1facfa0f98eac9fa3fc7
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2025-04-30 08:47:39 -0700

    solve Querying the Document C problem

--- c/querying_document.c
+++ c/querying_document.c
@@ -1,45 +1,53 @@
-// Solves: https://www.hackerrank.com/challenges/querying-the-document/problem?isFullScreen=true
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 #define MAX_CHARACTERS 1005
 #define MAX_PARAGRAPHS 5
 
-static char*** paragraphs[MAX_PARAGRAPHS];
-static char** sentences[MAX_CHARACTERS];
-static char* words[MAX_CHARACTERS];
+// START HACKERRANK PROVIDED CODE: CANT EDIT
+struct word {
+    char* data;
+};
 
-char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n);
-char** kth_sentence_in_mth_paragraph(char**** document, int k, int m);
-char*** kth_paragraph(char**** document, int k);
-char**** get_document(char* text);
+struct sentence {
+    struct word* data;
+    int word_count;//denotes number of words in a sentence
+};
 
-char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
-    char** sentence = kth_sentence_in_mth_paragraph(document, m, n);
-    // input is 1 indexed
-    return sentence[k-1];
-}
+struct paragraph {
+    struct sentence* data  ;
+    int sentence_count;//denotes number of sentences in a paragraph
+};
 
-char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) { 
-    char*** paragraph = kth_paragraph(document, m);
-    // input is 1 indexed
-    return paragraph[k-1];
-}
+struct document {
+    struct paragraph* data;
+    int paragraph_count;//denotes number of paragraphs in a document
+};
+// END HACKERRANK PROVIDED CODE: CANT EDIT
 
-char*** kth_paragraph(char**** document, int k) {
-    // input is 1 indexed
-    return document[k-1];
-}
+struct word kth_word_in_mth_sentence_of_nth_paragraph(struct document Doc, int k, int m, int n);
+struct sentence kth_sentence_in_mth_paragraph(struct document Doc, int k, int m);
+struct paragraph kth_paragraph(struct document Doc, int k);
 
-char**** get_document(char* text) {
+static struct paragraph paragraphs[MAX_PARAGRAPHS];
+static struct sentence sentences[MAX_CHARACTERS];
+static struct word words[MAX_CHARACTERS];
+
+struct document get_document(char* text) {
     char ch;
     int token_start = 0;
     int cur_idx = 0;
     int word_idx = 0;
     int sentence_idx = 0;
     int paragraph_idx = 0;
-    
-    // initialize first paragraph and sentence
-    sentences[sentence_idx] = &words[word_idx];
-    paragraphs[paragraph_idx] = &sentences[sentence_idx];
+    struct document result = {paragraphs, 0};
+
+    // initialize first paragraph and sentence.
+    sentences[sentence_idx].data = &words[word_idx];
+    sentences[sentence_idx].word_count = 0;
+    paragraphs[paragraph_idx].data = &sentences[sentence_idx];
+    paragraphs[paragraph_idx].sentence_count = 0;
 
     while (1)
     {
@@ -54,21 +62,26 @@ char**** get_document(char* text) {
             // or period.
             if (ch == ' ')
             {
-                words[word_idx++] = &text[token_start];
+                words[word_idx++].data = &text[token_start];
+                sentences[sentence_idx].word_count++;
             }
             else if (ch == '.')
             {
-                words[word_idx++] = &text[token_start];
+                words[word_idx++].data = &text[token_start];
+                sentences[sentence_idx].word_count++;
+                paragraphs[paragraph_idx].sentence_count++;
                 // prepare the next sentence
-                sentences[++sentence_idx] = &words[word_idx];
+                sentences[++sentence_idx].data = &words[word_idx];  
             }
             else if (ch == '\n')
             {
                 // prepare the next paragraph
-                paragraphs[++paragraph_idx] = &sentences[sentence_idx];
+                paragraphs[++paragraph_idx].data = &sentences[sentence_idx];
+                result.paragraph_count++;
             }   
             else if (ch == '\0')
             {
+                result.paragraph_count++;
                 break;
             }
             
@@ -79,5 +92,22 @@ char**** get_document(char* text) {
         cur_idx++;
     }
 
-    return paragraphs;
+    return result;
+}
+
+struct word kth_word_in_mth_sentence_of_nth_paragraph(struct document Doc, int k, int m, int n) {
+    struct sentence sen = kth_sentence_in_mth_paragraph(Doc, m, n);
+    // input is 1 indexed
+    return sen.data[k-1];
+}
+
+struct sentence kth_sentence_in_mth_paragraph(struct document Doc, int k, int m) { 
+    struct paragraph para = kth_paragraph(Doc, m);
+    // input is 1 indexed
+    return para.data[k-1];
+}
+
+struct paragraph kth_paragraph(struct document Doc, int k) {
+    // input is 1 indexed
+    return Doc.data[k-1];
 }
